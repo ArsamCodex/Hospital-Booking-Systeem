6dc08020d7bcda5cd198c6b5ceba2b8c481b6d76
Arii <arsamparsa777@gmail.com>
2021-02-04 19:39:10 +0100

    II

--- src/main/java/Hospital/System/HospitalBookingSysteemApplication.java
+++ src/main/java/Hospital/System/HospitalBookingSysteemApplication.java
@@ -1,16 +1,52 @@
 package Hospital.System;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class HospitalBookingSysteemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalBookingSysteemApplication.class, args);
+
+
+
+
+
+
+
+
+
+
+
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	// Enable jdbc authentication
+//	@Autowired
+//	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
+//	}
 }
